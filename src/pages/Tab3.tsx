2b8d8d78dca0ac83c898dc2eab298b08c50413a8
JUAN19742 <jsrueda@puce.edu.ec>
2026-06-16 23:43:07 -0500

    Lab-7

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,33 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil del Ususario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil del Ususario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/48026030?v=4" 
+            alt="Foto de Perfil" 
+             />
+             <IonCardHeader>
+              <IonCardTitle>Juan Rueda</IonCardTitle>
+              <IonCardSubtitle>JuanRueda</IonCardSubtitle>
+             </IonCardHeader>
+             <IonCardContent>
+              <p>Desarrollador de Software</p>
+              <p>Adicto a todo lo malo</p>
+             </IonCardContent>
+          </IonCard>
+        </div>
+
+        
       </IonContent>
     </IonPage>
   );
